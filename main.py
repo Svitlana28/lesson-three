48349f8fbf19a562a5eca0f85e5d65d51b585a4b
Svitlana28 <112892109+Svitlana28@users.noreply.github.com>
2023-07-07 19:28:04 +0300

    commit

---
+++ main.py
@@ -0,0 +1,29 @@
+day = int(input())
+if day == 1:
+    print("mandey")
+if day == 2:
+    print("tuesday")
+if day == 3:
+    print("wednesday")
+if day == 4:
+    print("thursday")
+if day == 5:
+    print("friday")
+if day == 6:
+    print("saturday")
+if day == 7:
+    print("sunday")
+
+#2D
+
+n1 = int(input(""))
+n2 = int(input(""))
+if (n1 == n2):
+    print(f"{n1}")
+if (n1 < n2):
+    print(f"{n1} , {n2} ")
+elif (n2 < n1):
+    print(f"{n2}  , {n1} ")
+
+    #3D
+
